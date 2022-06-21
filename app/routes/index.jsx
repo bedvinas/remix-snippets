d40298c6b55565af6bfb9cedf63b51b851b2fd06
Edvinas <bedvinas@outlook.com>
2022-06-21 02:55:03 +0200

    fix

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,3 +1,18 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
-	return <div>Hello Index Route</div>;
+	return (
+		<div className="container">
+			<div className="content">
+				<h1>SnippetCloud!!</h1>
+				<nav>
+					<ul>
+						<li>
+							<Link to="snippets">Check Snippets</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	);
 }
--- app/routes/snippets.jsx
+++ app/routes/snippets.jsx
@@ -1,11 +1,33 @@
-import { Outlet } from "@remix-run/react";
+import { Link, Outlet } from "@remix-run/react";
 
-export default function Snippets() {
+export default function JokesRoute() {
 	return (
 		<div>
-			<h1>Snippets</h1>
+			<header>
+				<div>
+					<h1>
+						<Link to="/" title="SnippetCloud" aria-label="Snippet Cloud">
+							<span>SNIPPETS</span>
+						</Link>
+					</h1>
+				</div>
+			</header>
 			<main>
-				<Outlet />
+				<div>
+					<div>
+						<Link to=".">Get a random snippet</Link>
+						<p>Here are a few more snippets to check out:</p>
+						<ul>
+							<li>
+								<Link to="some-snippet-id">Java - Product recomendation</Link>
+							</li>
+						</ul>
+						<Link to="new">Add your own snippet</Link>
+					</div>
+					<div>
+						<Outlet />
+					</div>
+				</div>
 			</main>
 		</div>
 	);
